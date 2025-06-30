ee10530fc5bbc2c8058dc2a857007b6c5a0ea39d
Supun Sulakshana <supunsulakshana2001@gmail.com>
2025-06-30 21:18:54 +0530

    Create AutoTimerSessionBean.java

---
+++ J2EE-Timer/src/main/java/lk/jiat/web/eetimer/ejb/AutoTimerSessionBean.java
@@ -0,0 +1,21 @@
+package lk.jiat.web.eetimer.ejb;
+
+import jakarta.ejb.Schedule;
+import jakarta.ejb.Schedules;
+import jakarta.ejb.Stateless;
+
+@Stateless
+public class AutoTimerSessionBean {
+
+    @Schedules({
+            @Schedule(hour = "21",minute = "3",second = "10" , persistent = false)  ,
+            @Schedule(hour = "21",minute = "3",second = "10" , persistent = false)  ,
+            @Schedule(hour = "21",minute = "3",second = "10" , persistent = false)  , 
+            @Schedule(hour = "21",minute = "3",second = "10" , persistent = false)
+    })
+
+
+    public void autoSchedule(){
+        System.out.println("autoSchedule...");
+    }
+}
